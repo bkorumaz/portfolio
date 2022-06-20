a35acb2d8987ac4f4036c31c0cd487f3511c50c0
Bahadır Korumaz <bahadir.korumaz@gmail.com>
2022-06-21 02:30:08 +0300

    add particles bacgkround

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,69 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.css';
+import Configs from './configurations.json';
+import ParticlesBg from 'particles-bg'
+import Typist from 'react-text-typist'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkBackgroundModes, setDarkBackgroundModes] = useState([
+    'day',
+    'terminal',
+    'torquoise',
+    'alizarin',
+    'amythyst',
+    'carrot',
+    'peterriver'
+  ])
+
+  const [lightBackgroundModes, setLightBackgroundModes] = useState([
+    'night',
+    'lightred',
+    'lightpurple',
+    'lightgreen',
+    'lightblue',
+    'lightyellow'
+  ])
+
+  const [backgroundType, setBackgroundType] = useState(Configs.backgroundType || 'plain')
+  const [appClass, setAppClass] = useState(Configs.plainBackgroundMode || 'daylight')
+  const [devIntro, setDevIntro] = useState(Configs.devIntro || 'Lorem Ipsum')
+  const [devDesc, setDevDesc] = useState(Configs.devDesc || 'Aute veniam ut deserunt cillum irure pariatur Lorem dolore anim nostrud quis veniam elit culpa.')
+  const [backgroundMode, setBackgroundMode] = useState('default')
+  const [backgroundIndex, setBackgroundIndex] = useState(0)
+  const [bgStyle, setBgStyle] = useState({})
+  const [icons, setIcons] = useState(Configs.icons || [])
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <div className={appClass} style={bgStyle}>
+        <div
+          className={backgroundMode}
+        >
+          <main className="App-main">
+            <ParticlesBg type="thick" bg={true} />
+            <div className="container">
+              <h1 className="intro">{devIntro}</h1>
+              <div className="shell">npm i your-lib --save</div>
+              <div className="tagline">
+                <Typist>{devDesc}</Typist>
+              </div>
+              
+              <div className="icons-social">
+                {icons.map((icon, i) => (
+                  <a
+                    target="_blank"
+                    key={i}
+                    rel="noopener noreferrer"
+                    href={`${icon.url}`}
+                  >
+                    <i className={`fab ${icon.image}`} />
+                  </a>
+                ))}
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
   )
 }
 
